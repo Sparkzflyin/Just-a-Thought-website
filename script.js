e543bd43f6fb8eb2f619336a5b34ce52ce6f13b7
Sparkzflyin <ctsparks15@gmail.com>
2026-03-24 18:47:14 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("body").insertAdjacentHTML("afterbegin", data);
+        });
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("body").insertAdjacentHTML("beforeend", data);
+        });
+});
